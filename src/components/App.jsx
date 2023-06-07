7b229f2597b95979602c0613e0ebe8a4c3ccd8e7
sashadubovyi <125696737+sashadubovyi@users.noreply.github.com>
2023-06-07 09:20:33 +0300

    React.lazy() та React.Suspense

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,31 @@
-import Header from 'Header/Header';
-import Home from 'Pages/Home/Home';
-import Cast from 'Pages/Сast/Cast';
-import MovieDetails from 'Pages/MovieDetails/MovieDetails';
-import Reviews from 'Pages/Reviews/Reviews';
-import Movies from 'Pages/Movies/Movies';
+import React, { lazy, Suspense } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { Body, Main } from './App.styled';
+
+const Header = lazy(() => import('Header/Header'));
+const Home = lazy(() => import('Pages/Home/Home'));
+const Cast = lazy(() => import('Pages/Сast/Cast'));
+const MovieDetails = lazy(() => import('Pages/MovieDetails/MovieDetails'));
+const Reviews = lazy(() => import('Pages/Reviews/Reviews'));
+const Movies = lazy(() => import('Pages/Movies/Movies'));
 
 export const App = () => {
   return (
     <Body>
-      <Header />
-      <Main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/movies/:movieId" element={<MovieDetails />}>
-            <Route path="cast" element={<Cast />} />
-            <Route path="reviews" element={<Reviews />} />
-          </Route>
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </Main>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Header />
+        <Main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/movies/:movieId" element={<MovieDetails />}>
+              <Route path="cast" element={<Cast />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </Main>
+      </Suspense>
     </Body>
   );
 };
